8d8d251f89d6dcc2d235b8a60074f109667b7498
Tammer Kamel <tammer@tammer.com>
2026-02-10 20:00:59 -0500

    .

--- evaluate.py
+++ evaluate.py
@@ -7,12 +7,29 @@ SYSTEM_PROMPT_FILE = Path(__file__).resolve().parent / "system_prompt.md"
 with open(SYSTEM_PROMPT_FILE, "r") as f:
     SYSTEM_PROMPT = f.read()
 
-def evaluate(profile: object) -> str:
+
+def check_for_problems(profile: object) -> dict:
+    if profile.get("location_country") != "Canada":
+        return {
+            "problem": "Not in Canada",
+            "id": profile.get("id"),
+            "total_score": 0,
+            "max_score": 11,
+        }
+    return None
+
+def evaluate(profile: object) -> dict:
     """
     Evaluate the profile and return the evaluation.
     """
-    response =  get_groq_response(
-        system_prompt=SYSTEM_PROMPT,
-        user_prompt=json.dumps(profile)
-    )
-    return json.loads(response)
+
+    problems = check_for_problems(profile)
+
+    if problems is None:
+        response =  get_groq_response(
+            system_prompt=SYSTEM_PROMPT,
+            user_prompt=json.dumps(profile)
+        )
+        return json.loads(response)
+    
+    return problems
