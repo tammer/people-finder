b90b6a3b415745f7b3f07139611af4a363d2c46c
Tammer Kamel <tammer@tammer.com>
2026-02-10 19:38:10 -0500

    .

--- view_response.py
+++ view_response.py
@@ -1,9 +1,12 @@
 import supa
 import json
+import sys
 
-id = 3313308
+# get the id from the command line
+id = int(sys.argv[1])
 
 # get the response from the responses table
 response = supa.read_response(id)
+
 
 print(json.dumps(response, indent=4))
